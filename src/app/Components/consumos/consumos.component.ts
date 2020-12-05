2327c9a7f96441a65a8184312e4cfbdec5a2f0a9
Matias Ogorinsky <matiogorinsky@gmail.com>
2020-12-05 15:14:44 -0300

    se-agrega-componente-consumos

---
+++ src/app/Components/consumos/consumos.component.ts
@@ -0,0 +1,11 @@
+import { Component} from '@angular/core';
+
+
+@Component({
+  selector: 'app-consumos',
+  templateUrl: './consumos.component.html',
+  styleUrls: ['./consumos.component.css']
+})
+export class ConsumosComponent{
+    
+}
