bb7a093aecbe04f0b40d54819ca77cac4253d419
Matias Ogorinsky <matiogorinsky@gmail.com>
2020-12-05 15:48:02 -0300

    se-agrega-tabla-ejemplo-consumos

--- src/app/Components/consumos/consumos.component.ts
+++ src/app/Components/consumos/consumos.component.ts
@@ -1,5 +1,18 @@
 import { Component} from '@angular/core';
 
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+}
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079 },
+  {position: 2, name: 'Helium', weight: 4.0026 },
+  {position: 3, name: 'Lithium', weight: 6.941 },
+  {position: 4, name: 'Beryllium', weight: 9.0122 },
+  {position: 5, name: 'Boron', weight: 10.811 },
+];
 
 @Component({
   selector: 'app-consumos',
@@ -8,4 +21,6 @@ import { Component} from '@angular/core';
 })
 export class ConsumosComponent{
     
+  displayedColumns: string[] = ['position', 'name', 'weight'];
+  dataSource = ELEMENT_DATA;
 }
