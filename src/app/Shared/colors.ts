140827214f1cab1d8c7bbb4526f25fd3c64b5145
nahuegallinoti <nahuel.gallinoti@incluit.com>
2020-12-16 16:43:04 -0300

    s

---
+++ src/app/Shared/colors.ts
@@ -0,0 +1,8 @@
+export class Colors {
+  public static readonly primario = '#3f51b5';
+  public static readonly secundario = '#B34FAE';
+  public static readonly terciario = '#F65D91';
+
+  public static readonly colorDebe = '#E9F73D';
+  public static readonly colorPagado = '#A8DF93';
+}
