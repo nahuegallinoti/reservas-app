ab2f8498d2808413f20f665817aa260a24634f83
Matias Ogorinsky <matiogorinsky@gmail.com>
2020-12-26 18:40:22 -0300

    se agrega productos

--- src/app/Components/alta-producto/alta-productos.component.ts
+++ src/app/Components/alta-producto/alta-productos.component.ts
@@ -1,14 +1,23 @@
-import {Component} from '@angular/core';
 
-export interface PeriodicElement {
-  descripcion: string;
-  precio: number;
-}
+import { AfterViewChecked } from '@angular/core';
+import { AfterViewInit } from '@angular/core';
+import { OnDestroy } from '@angular/core';
+import { Component, OnInit} from '@angular/core';
+import { Subscription } from 'rxjs';
+import { ViewChild } from '@angular/core';
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {descripcion: "Coca/cola", precio: 123},
+import { Producto } from 'src/app/Models/producto.model';
 
-];
+import { MatTableDataSource } from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+
+
+import { UIService } from 'src/app/Shared/ui.service';
+import { ProductoService } from 'src/app/Services/producto.service';
+
+
+
 
 @Component({
   selector: 'alta-productos',
@@ -16,8 +25,55 @@ const ELEMENT_DATA: PeriodicElement[] = [
   styleUrls: ['./alta-productos.component.css']
 })
 
-export class AltaProductosComponent{
-  Columnas: string[] = ['descripcion', 'precio'];
-  dataSource = ELEMENT_DATA;
+export class AltaProductosComponent implements OnInit, AfterViewInit, AfterViewChecked, OnDestroy{
   
+  isLoading = false;
+  productos = new MatTableDataSource<Producto>();
+  productosSubscription: Subscription;
+  isLoadingSubscription: Subscription;
+
+  private sort: MatSort;
+  private paginator: MatPaginator;
+
+  columnas: string[] = ['descripcion', 'precio'];
+
+  @ViewChild(MatSort) set matSort(ms: MatSort) {
+    this.sort = ms;
+    this.setDataSourceAttributes();
+  }
+  @ViewChild(MatPaginator) set matPaginator(mp: MatPaginator) {
+    this.paginator = mp;
+    this.setDataSourceAttributes();
+  }
+
+  setDataSourceAttributes() {
+    this.productos.sort = this.sort;
+    this.productos.paginator = this.paginator;
+  }
+
+  constructor(
+    private productoService: ProductoService,
+    private uiService: UIService
+  ) {}
+
+  ngOnInit(): void {
+    this.isLoadingSubscription = this.uiService.loadingStateChanged.subscribe(
+      (isLoading) => (this.isLoading = isLoading)
+    );
+    this.productoService.buscarProductos();
+  }
+
+  ngAfterViewInit(): void {
+    this.productos.sort = this.sort;
+  }
+
+  ngAfterViewChecked(): void {
+    if (!this.isLoading && this.isLoadingSubscription) {
+      this.isLoadingSubscription.unsubscribe();
+    }
+  }
+
+  ngOnDestroy(): void {
+  }
+
 }
