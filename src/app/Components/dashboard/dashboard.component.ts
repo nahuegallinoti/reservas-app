151163d84f2aaebb1653a2447b4ecaf5bab3d997
appify-ArsalanJaved <45562080+appify-ArsalanJaved@users.noreply.github.com>
2021-03-04 23:24:37 +0500

    //add chart and year filter

--- src/app/Components/dashboard/dashboard.component.ts
+++ src/app/Components/dashboard/dashboard.component.ts
@@ -10,17 +10,63 @@ import { Label, MultiDataSet } from 'ng2-charts';
 export class DashboardComponent implements OnInit {
 
   isLoading = false;
-
-  public barChartLabels: Label[] = ['Jun 2019', 'July 2019', 'Aug 2019', 'Sep 2019', 'Oct 2019', 'Nov 2019', 'Dec 2019'];
+  SelectedYear;
+  Years=[];
+  public barChartLabels: Label[] = ['Enero','Febrero','Marzo','Abril','Mayo','Jun', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'];
   public barChartType: ChartType = 'bar';
-  public barChartLegend = false;
+  public barChartLegend = true;
   public barChartPlugins = [];
-  public paymentVsChargeGraphData: ChartDataSets[] = [
-    { data: [20,50,70,90,110,130,150], label: 'Payments'},
-    { data: [30,40,60,80,100,120,140], label: 'Charges' }
+  public cabinData: ChartDataSets[] = [
+    { data: [20,50,70,90,110,130,150,70,90,110,130,150], label: 'Cab1'},
+    { data: [30,40,60,80,100,120,140,70,90,110,130,150], label: 'Cab2' },
+    { data: [30,40,60,80,100,120,140,70,90,110,130,150], label: 'Cab3' },
+    { data: [30,40,60,80,100,120,140,70,90,110,130,150], label: 'Cab4' },
+    { data: [10,30,50,70,90,110,130,150,170,190,210,230], label: 'Cab5' },
+    { data: [20,40,60,80,100,120,140,160,180,200,220,250], label: 'Cab6' }
   ];
   
-  public barChartOptions: ChartOptions = {
+  public productData: ChartDataSets[] = [
+    { data: [20,50,70,90,110,130,150,70,90,110,130,150], label: 'P1'},
+    { data: [30,40,60,80,100,120,140,70,90,110,130,150], label: 'P2' },
+    { data: [30,40,60,80,100,120,140,70,90,110,130,150], label: 'P3' },
+    { data: [30,40,60,80,100,120,140,70,90,110,130,150], label: 'P4' },
+    { data: [10,30,50,70,90,110,130,150,170,190,210,230], label: 'P5' },
+    { data: [20,40,60,80,100,120,140,160,180,200,220,250], label: 'P6' }
+  ];
+
+  public CabinbarChartOptions: ChartOptions = {
+    responsive: true,
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+      // callbacks: {
+      //   label: function(tooltipItems, data) {
+      //     return data.datasets[tooltipItems.datasetIndex].label + ": $" + parseFloat(tooltipItems.yLabel.toString()).toFixed(2);
+      // }
+      // }
+    },
+    legend:{
+      align:'start',
+      position: 'bottom',
+    },
+    scales: {
+      yAxes: [{
+          ticks: {
+              // Include a dollar sign in the ticks
+              // callback: function(value, index, values) {
+              //     return '$' + value;
+              // }
+          }
+      }]
+  },
+    hover: {
+      mode: 'nearest',
+      intersect: true
+    },
+    
+  };
+
+  public ProductbarChartOptions: ChartOptions = {
     responsive: true,
     tooltips: {
       mode: 'index',
@@ -33,6 +79,7 @@ export class DashboardComponent implements OnInit {
     },
     legend:{
       align:'start',
+      position: 'bottom',
     },
     scales: {
       yAxes: [{
@@ -50,30 +97,25 @@ export class DashboardComponent implements OnInit {
     },
     
   };
-  
-  public doughnutChartOptions: ChartOptions = {
-    responsive: true,
-    cutoutPercentage: 60,
-    tooltips: {
-      callbacks: {
-        label: function(tooltipItems, data) {
-          return data.labels[tooltipItems.index] + ": $" + parseFloat(data.datasets[tooltipItems.datasetIndex].data[tooltipItems.index].toString()).toFixed(2);
-      }
-      }
-    },
-  
-};
-public doughnutChartLabels: Label[] = ['Jan 2019', 'Feb 2019', 'Mar 2019', 'Apr 2019','May 2019','Jun 2019', 'July 2019', 'Aug 2019', 'Sep 2019', 'Oct 2019', 'Nov 2019', 'Dec 2019'];
-public collectedRevenueChartData: MultiDataSet = [
-  [20,56,78,28,88,99,23,45,12,25,67,55],
-];
-public doughnutChartType: ChartType = 'pie';
-public doughnutChartLegend = false;
 
   constructor() { }
 
   ngOnInit(): void {
 
+    this.getYearsList();
+  }
+
+  OnYearChange(){
+
+  }
+
+  getYearsList(){
+
+    var date = new Date();
+    this.SelectedYear = date.getFullYear();
+    for (let index = 0; index <= 10; index++) {
+       this.Years.push(date.getFullYear()-index);
+    }
   }
 
 }
