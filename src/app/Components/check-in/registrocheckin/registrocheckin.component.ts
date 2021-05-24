2539ad082c0cb5ed71819bf09d79e721fac0e947
Matias Ogorinsky <mogorinsky@ar0c02dw50aq05n.fibertel.com.ar>
2021-05-24 20:01:28 -0300

    hola

--- src/app/Components/check-in/registrocheckin/registrocheckin.component.ts
+++ src/app/Components/check-in/registrocheckin/registrocheckin.component.ts
@@ -103,7 +103,7 @@ export class RegistrocheckinComponent implements OnInit {
     value: "",
     text: "",
   };
-
+// sdsd
 
   solicitudesReservaSubscription: Subscription;
   solicitudesReserva: Reserva[] = [];
